472a26ab9e2916aa2007c817afaf218e784c3a31
nazif-malhi <nazifmalhi@gmail.com>
2022-12-11 23:13:43 +0500

    pages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import MockUp from "./components/pages/MockUp";
 // import { MockUp } from "./components/pages";
 import NavbarHeader2 from "./components/navbar/NavbarHeader2";
 import Footer from "./components/layouts/Footer";
+import Crop_Recomendation from "./components/detection_inputs/Crop_Recomendation";
+import Crop_Disease_Detection from "./components/detection_inputs/Crop_Disease_Detection";
 
 function App() {
   return (
     <>
-      <NavbarHeader2/>
+      <NavbarHeader2 />
       <Routes>
         <Route path="/" element={<MockUp />} />
+        <Route path="/models/*">
+          <Route path="crop_recomendation" element={<Crop_Recomendation />} />
+          <Route
+            path="crop_disease_detection"
+            element={<Crop_Disease_Detection />}
+          />
+          <Route path="pest_detection" element={<Crop_Recomendation />} />
+          <Route path="crop_recomendation" element={<Crop_Recomendation />} />
+        </Route>
       </Routes>
-      <Footer/>
+      <Footer />
     </>
   );
 }
---
+++ src/components/detection_inputs/Fertilizer_Recomendation.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Fertilizer_Recomendation = () => {
+  return <div>Fertilizer_Recomendation</div>;
+};
+
+export default Fertilizer_Recomendation;
